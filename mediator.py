1315baa1b490cca572b92061d616e31d2d2f1546
Erik Pohl <epohl@dmigs.com>
2025-01-28 22:44:26 -0500

    refined the mediator pattern code

--- mediator.py
+++ mediator.py
@@ -8,21 +8,29 @@ each other explicitly, and it lets you vary their interaction independently."
 """
 
 
+class Cell:
+    def __init__(self):
+        self.status = random.randint(0, 1)
+
+    def am_i_alive(self):
+        return self.status
+
+    def set_status(self, x):
+        self.status = x
+
+
 class LifeCellGridMediator:
     """
     Cooperative behavior is permitted by coordinating Colleague cell objects.
     The mediator knows and maintains all Colleague cell objects.
     """
 
-    def __init__(self):
+    def __init__(self, xbound, ybound):
         print("creating grid mediator")
-        self.maxx = 3
-        self.maxy = 3
-        self.cell = [[0 for _ in range(self.maxx)] for _ in range(self.maxy)]
+        self.maxx = xbound
+        self.maxy = ybound
+        self.cells = [[Cell() for _ in range(self.maxx)] for _ in range(self.maxy)]
         self.clock = 0
-        for y in range(self.maxy):
-            for x in range(self.maxx):
-                self.cell[x][y] = Cell(self)
 
     def clock_tick(self):
         print("clock tick!")
@@ -31,46 +39,30 @@ class LifeCellGridMediator:
         y = random.randint(0, self.maxy-1)
         above = 0
         if y > 0:
-            abovecell = self.cell[x][y-1]
-            above = abovecell.amialive()
+            above = self.cells[x][y - 1].am_i_alive()
         below = 0
         if y < self.maxy-1:
-            belowcell = self.cell[x][y+1]
-            below = belowcell.amialive()
+            below = self.cells[x][y + 1].am_i_alive()
         left = 0
         if x > 0:
-            leftcell = self.cell[x-1][y]
-            left = leftcell.amialive()
+            left = self.cells[x - 1][y].am_i_alive()
         right = 0
         if x < self.maxx-1:
-            rightcell = self.cell[x+1][y]
-            right = rightcell.amialive()
+            right = self.cells[x + 1][y].am_i_alive()
 
-        self.cell[x][y].setstatus(int(above+below+left+right))
+        self.cells[x][y].set_status(int(above + below + left + right))
 
     def print_out(self):
         print("clock at ", self.clock, " seconds")
         for y in range(self.maxy):
             line = ""
             for x in range(self.maxx):
-                line += str(self.cell[x][y].amialive())
+                line += str(self.cells[x][y].am_i_alive())
             print(line)
 
 
-class Cell:
-    def __init__(self, mediator):
-        self._mediator = mediator
-        self.status = random.randint(0, 1)
-
-    def amialive(self):
-        return self.status
-
-    def setstatus(self, x):
-        self.status = x
-
-
 def main():
-    mediator = LifeCellGridMediator()
+    mediator = LifeCellGridMediator(3, 3)
     mediator.print_out()
     for i in range(10):
         mediator.clock_tick()
